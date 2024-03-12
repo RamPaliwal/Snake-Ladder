20870d490ba76f66dd56ae48225bc823b417bbce
RamPaliwal <rampaliwal2002@gmail.com>
2024-03-12 10:50:10 +0530

    Added UC 4

--- Snake_Ladder.java
+++ Snake_Ladder.java
@@ -4,23 +4,26 @@ public class Snake_Ladder {
 	int position=0;
         int Ladder=1;
         int Snake=2;
-        int option=(int) Math.floor(Math.random()*10)%3;
-        int num = (int) Math.floor(Math.random()*10)%6+1;
-        if(option==Ladder){
-            position += num;
-            System.out.println("Selected option is Ladder");
-            System.out.println("You are at position "+position);
-        }
-        else if(option==Snake){
-            if(position>=num){
-            position -= num;
+        while(position<=100){
+            int option=(int) Math.floor(Math.random()*10)%3;
+            int num = (int) Math.floor(Math.random()*10)%6+1;
+            if(option==Ladder){
+                position += num;
+                System.out.println("Selected option is Ladder");
+                System.out.println("You are at position "+position);
             }
-            System.out.println("Selected option is Snake");
-            System.out.println("You are at position "+position);
+            else if(option==Snake){
+                if(position>=num){
+                    position -= num;
+                }
+                System.out.println("Selected option is Snake");
+                System.out.println("You are at position "+position);
+            }
+            else{
+                System.out.println("Selected option is No Play");
+                System.out.println("You are at position "+position);
+            }
         }
-        else{
-            System.out.println("Selected option is No Play");
-            System.out.println("You are at position "+position);
-        }
+        System.out.println("You win");
     }
 }
