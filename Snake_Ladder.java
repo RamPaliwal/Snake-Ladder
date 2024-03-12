121b74bce256741fb9f57a660fe25386715b5ca9
RamPaliwal <rampaliwal2002@gmail.com>
2024-03-12 10:36:00 +0530

    Added UC 1

---
+++ Snake_Ladder.java
@@ -0,0 +1,7 @@
+public class Snake_Ladder {
+    public static void main(String[] args) {
+        int start_position = 0;
+        System.out.println("Welcome to snake & ladder game");
+        System.out.println("You are at position "+start_position);
+    }
+}
