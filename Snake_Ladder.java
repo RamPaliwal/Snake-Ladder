5e9a6d93029fe9ebfa48cbf5407568c617fe8595
RamPaliwal <rampaliwal2002@gmail.com>
2024-03-12 10:56:18 +0530

    Added UC 6 for Dice Count

--- Snake_Ladder.java
+++ Snake_Ladder.java
@@ -1,10 +1,11 @@
 public class Snake_Ladder {
     public static void main(String[] args) {
-        System.out.println("Welcome to snake & ladder game");
-	int position=0;
+        int position=0;
         int Ladder=1;
         int Snake=2;
+        int dice_count=0;
         while(position<=100){
+            dice_count+=1;
             int option=(int) Math.floor(Math.random()*10)%3;
             int num = (int) Math.floor(Math.random()*10)%6+1;
             if(option==Ladder){
@@ -13,6 +14,7 @@ public class Snake_Ladder {
                 }
                 System.out.println("Selected option is Ladder");
                 System.out.println("You are at position "+position);
+                System.out.println();
             }
             else if(option==Snake){
                 if(position>=num){
@@ -20,15 +22,18 @@ public class Snake_Ladder {
                 }
                 System.out.println("Selected option is Snake");
                 System.out.println("You are at position "+position);
+                System.out.println();
             }
             else{
                 System.out.println("Selected option is No Play");
                 System.out.println("You are at position "+position);
+                System.out.println();
             }
             if(position==100){
                 break;
             }
         }
         System.out.println("You win");
+        System.out.println("Total dice count is "+dice_count);
     }
 }
