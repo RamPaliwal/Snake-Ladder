10906c92b79303a26fc15371477fa2404b9f06a7
RamPaliwal <rampaliwal2002@gmail.com>
2024-03-12 10:42:54 +0530

    Added UC 2 to roll the die

--- Snake_Ladder.java
+++ Snake_Ladder.java
@@ -3,5 +3,8 @@ public class Snake_Ladder {
         int start_position = 0;
         System.out.println("Welcome to snake & ladder game");
         System.out.println("You are at position "+start_position);
+	int num = (int) Math.floor(Math.random()*10)%6+1;
+        System.out.println("You got "+num+" on die");
+
     }
 }
